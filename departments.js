8bc7100df2a5de124afecff8a8f4f25da63c0065
Ajn0r <ronjasjogren@hotmail.com>
2024-02-23 07:41:59 +0100

    add links to the navy text

--- departments.js
+++ departments.js
@@ -6,9 +6,10 @@ const handleDepartments = async(department, language) => {
         } else if (department === 'Flygvapnet') {
             return 'Flygvapnet är en av Försvarsmaktens tre huvudgrenar. Flygvapnet ansvarar för att upprätthålla luftrumssuveräniteten över Sverige och för att kunna genomföra flyginsatser i hela landet.';
         } else if (department === 'Marinen') {
-            return 'Marinen är en av Försvarsmaktens tre huvudgrenar. Marinen ansvarar för att skydda Sveriges kuster och för att kunna genomföra insatser till sjöss.';
+            return 'Marinen är en av Försvarsmaktens tre huvudgrenar. Marinen ansvarar för att skydda Sveriges kuster och för att kunna genomföra insatser till sjöss.' + ' ' +
+                'Vill du veta med om Marinen kan du kika in <här href="https://www.forsvarsmakten.se/sv/var-verksamhet/det-har-gor-forsvarsmakten/marinen">här</a>';
         } else {
-            return `Jag förstår inte frågan. Ställ en annan fråga eller kontakta Försvarsmakten via telefon: 08 - 788 75 00 eller e-post:  exp-hkv@mil.se.`;
+            return 'Försvarsmakten har tre huvudgrenar: Armén, Flygvapnet och Marinen. Vilken gren vill du veta mer om?';
         }
     } else if (language === 'en') {
 
@@ -17,10 +18,11 @@ const handleDepartments = async(department, language) => {
         } else if (department === 'Flygvapnet') {
             return 'The Air Force is one of the three main branches of the Swedish Armed Forces. The Air Force is responsible for maintaining air space sovereignty over Sweden and for being able to carry out air operations throughout the country.';
         } else if (department === 'Marinen') {
-            return 'The Navy is one of the three main branches of the Swedish Armed Forces. The Navy is responsible for protecting Sweden\'s coasts and for being able to carry out operations at sea.';
+            return 'The Navy is one of the three main branches of the Swedish Armed Forces. The Navy is responsible for protecting Sweden\'s coasts and for being able to carry out operations at sea.' + ' ' +
+                'If you want to know more about the Navy, you can check it out <a ref="https://www.forsvarsmakten.se/en/about/organisation/the-navy/" >here</a>';
         }
     } else {
-        return `I don't understand the question. Ask another question or contact the Swedish Armed Forces via phone: 08 - 788 75 00 or e-mail:  exp-hkv@mil.se.`;
+        return 'The armed forces have three main branches: the Army, the Air Force and the Navy. Which branch would you like to know more about?';
     }
 };
 
